a66668a60a0c89061b4bba1dcb46133eb88331fa
Victor D. Sandiego <codepuppy@balticsoap.com>
2022-06-16 11:57:48 -0500

    Code cleanup

--- src/Restless.Tiingo/Data/CryptoMetaData.cs
+++ src/Restless.Tiingo/Data/CryptoMetaData.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Restless.Tiingo.Data
+﻿namespace Restless.Tiingo.Data
 {
     public class CryptoMetaData : CurrencyPairMetaData
     {
